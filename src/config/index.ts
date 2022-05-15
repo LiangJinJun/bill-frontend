caddb5ba10707e636616777523d0e2bc3ccdcfb6
liangjinjun <2210258654@qq.com>
2022-05-16 01:10:55 +0800

    fix: file saving in mine

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,9 @@
 export const FANG_CUN = 'http://192.168.0.105:3001';
 export const JIN_JUN_SERVER = 'http://120.77.182.207:3001';
 export const REACT_XIAO_MI = 'http://192.168.31.201:3001';
+export const MCYJ324 = 'http://192.168.0.174:3001';
 export const LOCAL_SERVER = 'http://localhost:3001';
 
-const defaultHost = JIN_JUN_SERVER;
+const defaultHost = MCYJ324;
 
 export { defaultHost };
